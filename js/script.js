21e29f39d5c6a0d64336f149d894797236508b9f
omarf4650@gmail.com <omarf4650@gmail.com>
2023-10-23 22:49:13 +0600

    Okey?

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('Testing console log');
+window.alert('Be careful. This website is harmful');
+var x = prompt('enter your mail address');
+document.write(x);
+console.log(x);
+document.getElementById('c').innerHTML = 'innerHtml testing';
+
+
+var a = "faruk";
+var b = "21+";
+let c = 23;
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = 20;
+var b = a;
+console.log(a);
+console.log(b);
+var a = 50
+console.log(a);
+console.log(b);
